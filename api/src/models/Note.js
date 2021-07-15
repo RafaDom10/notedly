4ec1a33252c6017d283128fd05b74a1620fd05d2
RafaDom10 <r-domingues@outlook.com>
2021-07-15 19:47:30 -0300

    crud done

---
+++ api/src/models/Note.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+
+);
+
+module.exports = mongoose.model('Note', NoteSchema);
